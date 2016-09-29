e77f93663299a05c5d9eea073022332d9ead0a35
“Jesus <jesusdsaravia@gmail.com>
2016-09-29 15:21:43 -0500

    un commit mas

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -409,8 +409,8 @@ public class Principal extends javax.swing.JFrame {
     }//GEN-LAST:event_txtNcolumnas1KeyTyped
 
     /**
-         * @param args the command line arguments
-         */
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
